3b62ac02d14c266bee3ddcc1ac33358d1b413298
ZohaibArshad <zohaibarshad2237@gmail.com>
2022-01-17 20:19:34 +0500

    prepared for deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,32 @@
 export default {
+  buildDir: "nuxt-dist",
   head: {
-
-    link: [{ rel:"stylesheet", type:"text/css", href:'https://vjs.zencdn.net/7.11.4/video-js.css' }],
-    link: [{ rel:"stylesheet", type:"text/css", href:'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' }],
+    link: [
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://vjs.zencdn.net/7.11.4/video-js.css",
+      },
+    ],
+    link: [
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+      },
+    ],
   },
   plugins: [
-    { src: '~plugins/amplify.js'},
-    { src: '~/plugins/auth'},
-    { src: '~/plugins/stripe.js', ssr: false }
+    { src: "~plugins/amplify.js" },
+    { src: "~/plugins/auth" },
+    { src: "~/plugins/stripe.js", ssr: false },
   ],
-  css: [
-    "~layouts/global.css",
-  ],
+  css: ["~layouts/global.css"],
   buildModules: [
-    '@nuxtjs/vuetify',
-    '@nuxtjs/dotenv',
-    '@nuxtjs/axios',
-    'nuxt-stripe-module'
-
+    "@nuxtjs/vuetify",
+    "@nuxtjs/dotenv",
+    "@nuxtjs/axios",
+    "nuxt-stripe-module",
   ],
   env: {
     STRIPE_PK: process.env.STRIPE_PK,
@@ -25,7 +34,6 @@ export default {
   publicRuntimeConfig: {
     stripe: {
       publishableKey: process.env.STRIPE_PK,
-    }
+    },
   },
-
-}
+};
